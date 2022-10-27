352791b852df2778f13966a09819cc1f2b9a5201
Paul Mourer <pmourer@joinhandshake.com>
2022-10-27 09:17:13 -0600

    Still not working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react"
-import { Switch } from '@headlessui/react'
+import { Switch } from "@headlessui/react"
 
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen min-w-full">
-      <Switch
-        className="relative inline-flex h-6 w-10 items-center rounded-full ui-checked:bg-blue-600 bg-gray-200"
-      >
+      <Switch className="relative inline-flex h-6 w-10 items-center rounded-full ui-checked:bg-blue-600 bg-gray-200">
         <span className="sr-only">Enable notifications</span>
         <span className="inline-block h-4 w-4 transform rounded-full bg-white transition ui-checked:translate-x-6 translate-x-1" />
       </Switch>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  important: true,
-  //Purging for Production is configured in PostCSS Config
-  //purge:{    
-  //  content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
-  //},
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [require("@headlessui/tailwindcss")],
+  plugins: [require('@headlessui/tailwindcss')],
 };
